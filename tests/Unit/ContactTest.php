2985833e7d1dfff86a99506aa00afd4b9a169f4d
RomarioGiacholari <giacholari@gmail.com>
2018-07-26 21:30:02 +0100

    change welocme view remove comments from web.php and refactor contact test

--- tests/Unit/ContactTest.php
+++ tests/Unit/ContactTest.php
@@ -10,81 +10,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ContactTest extends TestCase
 {
     use RefreshDatabase;
-    
-    protected $user;
 
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->user = create(User::class);
-    }
-
-    public function test_it_returns_its_path()
+    public function test_a_contact_can_make_a_string_path()
     {
         $contact = create(Contact::class);
 
-        $this->assertEquals("/contacts/{$contact->id}", $contact->path());
+        $this->assertEquals($contact->path(), "/contacts/{$contact->id}");
     }
 
-    public function test_it_belongs_to_a_user()
+    public function test_a_contact_belongs_to_a_user()
     {
-        $contact = create(Contact::class, [
-            'user_id' => $this->user->id
-        ]);
+        $this->actingAs($user = create(User::class));
 
-        $this->assertEquals($this->user->name, $contact->user->name);
+        $contact = make(Contact::class);
+
+        $user->contacts()->save($contact);
+
+        $this->assertTrue($user->contacts->contains($contact)); 
     }
-
-    public function test_an_authenticated_user_can_save_a_contact()
-    {
-        $this->withoutExceptionHandling();
-        
-        $this->actingAs($this->user);
-
-        $this->post(route('contacts.store'), ['name' => 'James']);
-
-        $this->assertDatabaseHas('contacts', ['name' => 'James']);
-    }
-
-    public function test_an_anauthenticated_user_cannot_save_a_contact()
-    {
-        $response = $this->post(route('contacts.store'), ['name' => 'James']);
-
-        $response->assertRedirect('/login'); 
-    }
-
-    public function test_an_authenticated_user_can_update_a_contact()
-    {
-        $this->withoutExceptionHandling();
-        
-        $this->actingAs($this->user);
-
-        $contact = create(Contact::class, [
-            'user_id' => $this->user->id,
-            'name' => 'Michael'
-        ]);
-
-        $this->put(route('contacts.update', $contact->id), ['name' => 'John']);
-
-        $this->assertEquals('John', $contact->fresh()->name);
-    }
-
-    public function test_an_authenticated_user_can_delete_a_contact()
-    {
-        $this->withoutExceptionHandling();
-        
-        $this->actingAs($this->user);
-
-        $contact = factory(Contact::class)->create([
-            'user_id' => $this->user->id,
-        ]);
-
-        $this->assertCount(1, $this->user->contacts);
-
-        $this->delete(route('contacts.destroy', $contact->id));
-
-        $this->assertCount(0, $this->user->fresh()->contacts);
-    }
-
 }
